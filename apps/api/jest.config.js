85c2685d42784881830c0569a1921eefcf5287e4
Nicolazic <nicolazictardy@gmail.com>
2026-01-29 03:00:10 +0100

    test(api): configure Jest for NestJS testing
    - Add jest.config.js with TypeScript and NestJS support
    - Add test dependencies (@nestjs/testing, jest, ts-jest)

---
+++ apps/api/jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['src/**/*.(t|j)s', '!src/main.ts', '!src/**/*.module.ts'],
+  coverageDirectory: './coverage',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src/', '<rootDir>/test/'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@art-et-jardin/database$': '<rootDir>/../../packages/database/src/index.ts',
+    '^@art-et-jardin/shared$': '<rootDir>/../../packages/shared/src/index.ts',
+  },
+  setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
+  testTimeout: 30000,
+};
